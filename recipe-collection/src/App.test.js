c3fe691663023cc650c065cac1cd9d7727b17ca7
dpark93 <d.park.official@gmail.com>
2023-10-30 13:24:39 -0600

    red phase of the TDD

---
+++ recipe-collection/src/App.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+
+test('As a Chef, I want to store my recipes so that I can keep track of them.', () => {
+  render(<App />);
+
+  let recipeHeader = screen.getByText('My Recipes');
+  expect(recipeHeader).toBeInTheDocument();
+
+  let recipeList = screen.getByText('There are no recipes to list.');
+  expect(recipeList).toBeInTheDocument();
+
+
+  expect(recipeHeader.compareDocumentPosition(recipeList)).toBe(4);
+});
